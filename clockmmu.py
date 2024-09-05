50e7082f9b36200bb36256e82ba295e305bfb831
Erfan5555 <109932909+Erfan5555@users.noreply.github.com>
2024-09-05 12:21:12 +0930

    finished the clock alg

--- clockmmu.py
+++ clockmmu.py
@@ -1,24 +1,28 @@
+
+
+
 from mmu import MMU
 
-class page:
-    def __init__(self,page_num):
+class Page:
+    def __init__(self, page_num):
         self.page_num = page_num
-        self.ref_bit =0
-        self.dirty= False
+        self.ref_bit = 0
+        self.dirty = False
 
     def __repr__(self):
-        return f"Page({self.page_number}, ref_bit={self.reference_bit}, dirty={self.dirty})"
+        return f"Page({self.page_num}, ref_bit={self.ref_bit}, dirty={self.dirty})"
+
 
 
 class ClockMMU(MMU):
-    def __init__(self, frames):
-        self.frames= [None]* frames
+    def __init__(self, frames_num):
+        self.frames= [None]* frames_num
         self.clock_tip = 0
         self.count_diskR= 0
         self.count_diskW=0
-        self.count_page_faults=0
+        self.page_faults=0
         self.debug_mode=False
-        self.total_frames= frames
+        self.total_frames= frames_num
 
     def set_debug(self):
         self.debug_mode = True
@@ -26,48 +30,63 @@ class ClockMMU(MMU):
     def reset_debug(self):
         self.debug_mode = False
     
+    def handle_empty (self, page_number,write,current):
+        # set the ref bit to 1 
+        # if its a write op set the dirty bit to treu - Check THIS WITH THE BOOK
+        self.frames[self.clock_tip] = Page(page_number)
+        self.frames[self.clock_tip].ref_bit = 1  
 
-    def clock_first_pass(self,page_number):
+        if write:
+            self.frames[self.clock_tip].dirty = True  
+        self.count_diskR += 1  
+        if self.debug_mode:
+            print(f"load page{current}")
+        self.clock_tip = (self.clock_tip + 1) % len(self.frames) 
+        return current
+    
 
+    def handle_zero (self, page_number,write,current):
+        current.ref_bit = 0
+        if self.debug_mode:
+            print("pass making the ref bit 0 now")
+        self.clock_tip = (self.clock_tip + 1) % len(self.frames)  
+        return
+
+    def handle_replacment (self, page_number,write,current):
+        if current.dirty:
+
+            self.count_diskW += 1
+            if self.debug_mode:
+                print(f"writing dirty page{current}")
+
+        if self.debug_mode:
+            print(f"Evicting {current}")
+        self.frames[self.clock_tip] = Page(page_number)
+        self.frames[self.clock_tip].ref_bit = 1  
+        if write:
+            self.frames[self.clock_tip].dirty = True 
+        self.count_diskR += 1  
+        if self.debug_mode:
+            print(f"Loading page{current} ")
+        self.clock_tip = (self.clock_tip + 1) % len(self.frames)  
+        return
+
+
+    def clock(self, page_number,write):
+
+        while True:
             current = self.frames[self.clock_tip]
             if current is None:
-                self.frames[self.clock_tip] = current.page_num
-                self.frames[self.clock_tip].ref_bit=1
-                self.count_diskR+=1
-                if self.debug_mode:
-                    print("loading page")
-                self.clock_tip=(self.clock_tip+1)% self.num_frames
-                return -1
-            else :
-                if current.ref_bit==0 and current.dirty== False:
-                    self.frames[self.clock_hand] = Page(page_number)
-                    self.frames[self.clock_hand].reference_bit = 1
-                    self.disk_reads += 1
-                    self.clock_hand = (self.clock_hand + 1) % self.num_frames
-                    return -1
-                else:
-                    current.reference_bit = 0
-                    self.clock_hand = (self.clock_hand + 1) % self.num_frames
-                    return +1
-    
-    def sec_clock_pass(self,page_number):
-        
-    
-    
-    def clock(self,page_number):
-        
-        while True:
-            a= self.clock_first_pass(self,page_number)
-            if a== -1:
+                self.handle_empty ( page_number,write,current)
                 return
             else:
-                a= self.sec_clock_pass(self,page_number)
+                if current.ref_bit == 1:
+                    self.handle_zero(page_number,write,current)
 
-            
-
-
-
-
+                else:
+                    self.handle_replacment (page_number,write,current)
+           
+                    return
 
 
 
@@ -75,12 +94,12 @@ class ClockMMU(MMU):
 
         for page in self.frames:
             if page and page.page_num == page_number: 
-                    page.ref_bit==1
+                    page.ref_bit=1
                     if self.debug_mode:
                         print("Read hit")
                     return
         self.page_faults+=1
-        self.clock(page_number)
+        self.clock(page_number,write=False)
 
     def write_memory(self, page_number):
 
@@ -95,26 +114,15 @@ class ClockMMU(MMU):
                 return
 
         self.page_faults+=1
-        self.clock(page_number)
-
+        self.clock(page_number,write=True)
 
 
     def get_total_disk_reads(self):
-        # TODO: Implement the method to get total disk reads
-        return -1
+        return self.count_diskR
 
     def get_total_disk_writes(self):
-        # TODO: Implement the method to get total disk writes
-        return -1
+        return self.count_diskW
 
     def get_total_page_faults(self):
-        # TODO: Implement the method to get total page faults
-        return -1
+        return self.page_faults
 
-
-# approx least recently used  initilaly set all the refernces to 1 then 
-# if the refenrce bit is 0, the page is chosen for replacemnt 
-# if the ref bit is 1, the bit is cleared and the clock hand moves to the next page
-
-# disk reads loading a page from disk 
-# disk writes saving a modified page back to disk before replacing it 
